8b2c1893885be848e02c4727ca28cf12a010857d
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2022-11-15 23:05:17 +0100

    It works!

--- dodeca/dodeca.adb
+++ dodeca/dodeca.adb
@@ -1,55 +1,57 @@
 --dodeca.adb
 
-
 with Ada.Text_IO;
 use  Ada.Text_IO;
 
 procedure DODECA is
-   N : constant := 3;   -- 12 when debuged!
+   SERIES_LENGTH : constant := 12;
 
-   type Note_Type     is range 1..N;
-   type Interval_Type is range 1..N-1;
+   type Note_Type     is range 1..SERIES_LENGTH;
+   type Interval_Type is range 1..SERIES_LENGTH-1;
+   type Level_Type    is range 1..SERIES_LENGTH;
 
-   notes     : array (Note_Type) of Boolean := (others => False);
-   intervals : array (Interval_Type) of Boolean := (others => False);
+   type Solution_Type is array (Level_Type) of Note_Type;
 
-   interval  : Interval_Type;
+   -- Notes and intervals not yet used
+   Notes     : array (Note_Type) of Boolean := (others => True);
+   Intervals : array (Interval_Type) of Boolean := (others => True);
 
-   subtype Level_Type is Integer range 1..N;
-   series : array (Level_Type) of Note_Type;
+   -- Vector with solution
+   Solution  : Solution_Type;
 
    procedure Advance(level: Level_Type) is
+      interval : Interval_Type;
    begin
       for note in Note_Type loop
-         if not notes(note) then
-            notes(note) := True;
-            interval := Interval_Type(abs (note - series(level-1)));
-            if not intervals(interval) then
-               intervals(interval) := True;
-               series(level) := note;
-
-               if level < N then
-                  Advance(level + 1);
-               else
-                  -- one complete series found
-                  for note of series loop
+         -- is note free to take?
+         if Notes(note) then
+            interval := Interval_Type(abs (note - Solution(level-1)));
+            -- is interval free to take?
+            if Intervals(interval) then
+               Solution(level) := note;
+               if level = SERIES_LENGTH then
+                  -- one complete solution found
+                  for note of Solution loop
                      put(note'Image);
                   end loop;
                   New_Line;
+               else
+                  Notes(note)         := False;
+                  Intervals(interval) := False;
+                  Advance(level + 1);
+                  Intervals(interval) := True;
+                  Notes(note)         := True;
                end if;
-
-               intervals(interval) := False;
             end if;
-            notes(note) := False;
          end if;
       end loop;
    end Advance;
 begin
    for note in Note_Type loop
-      notes(note) := True;
-      series(1) := note;
+      Solution(1) := note;
+      Notes(note) := False;
       Advance(2);
-      notes(note) := False;
+      Notes(note) := True;
    end loop;
 end DODECA;
 
