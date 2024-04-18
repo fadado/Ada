2511ccfada61ee5cd4e0c8e9edd0307d1bafa74f
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2024-04-18 09:44:30 +0200

    Compiling as a library

---
+++ kiss/dummy.adb
@@ -0,0 +1,26 @@
+-- dummy.adb
+
+
+with kiss;
+with kiss.functors;
+with kiss.functors.deque;
+with kiss.functors.queue;
+with kiss.functors.stack;
+with kiss.interfaces;
+with kiss.interfaces.deque;
+with kiss.interfaces.queue;
+with kiss.interfaces.stack;
+with kiss.signatures;
+with kiss.signatures.deque;
+with kiss.signatures.queue;
+with kiss.signatures.stack;
+
+procedure dummy is
+begin
+   null;
+end dummy;
+
+-- ¡ISO-8859-1!
+-- vim:tabstop=3:shiftwidth=3:expandtab:autoindent
+-- vim:fileformat=dos:fileencoding=latin1:syntax=ada
+
