b7cb80a5b975264c4d25555b5337e24e7b372de1
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2024-04-23 09:29:44 +0200

    Separate output

---
+++ dodeca/dodeca-output.adb
@@ -0,0 +1,19 @@
+-- dodeca-output.adb
+
+with Ada.Text_IO;
+
+separate (Dodeca)
+procedure Output(Full: SOLUTION)
+is
+   use Ada.Text_IO;
+begin
+   for item of Full loop
+      Put(item'Image);
+   end loop;
+   New_Line;
+end;
+
+
+-- ¡ISO-8859-1!
+-- vim:tabstop=3:shiftwidth=3:expandtab:autoindent
+-- vim:fileformat=dos:fileencoding=latin1:syntax=ada
--- dodeca/dodeca.adb
+++ dodeca/dodeca.adb
@@ -1,44 +1,34 @@
 -- dodeca.adb
 
-with Ada.Text_IO;
-
-procedure dodeca is
+procedure Dodeca is
    pragma Optimize(Time);
 
    Chromatic_Notes: constant := 12;
 
+   type CHOICE is range 1 .. Chromatic_Notes;
    -- Set of available choices
-   type CHOICE   is range 1 .. Chromatic_Notes;
-   type LEVEL    is new CHOICE;
 
-   -- Ordered set of choices
+   type LEVEL is new CHOICE;
+   -- Search tree level
+
    type SOLUTION is array (LEVEL) of CHOICE;
+   -- Ordered set of choices
 
-------------------------------------------------------------------------
-   -- Consumer
-------------------------------------------------------------------------
-   procedure output(vector: SOLUTION)
-   is
-      use Ada.Text_IO;
-   begin
-      for item of vector loop
-         Put(item'Image);
-      end loop;
-      New_Line;
-   end;
-------------------------------------------------------------------------
-   -- Producer
-------------------------------------------------------------------------
-   -- Vector with current (partial) solution
-   serie: SOLUTION;
+   procedure Output(Full: SOLUTION) is separate;
+   -- Called for each solution
 
-   -- Sets of notes and intervals used
    type INTERVAL is range CHOICE'First .. CHOICE'Pred(CHOICE'Last);
-   used_notes     : array (CHOICE)   of BOOLEAN := (others => FALSE);
-   used_intervals : array (INTERVAL) of BOOLEAN := (others => FALSE);
+   -- Constraining intervals (must be unique)
 
-   -- Try to add one partial solution
-   procedure extend(current: LEVEL) is
+   Used_Notes     : array (CHOICE)   of BOOLEAN := (others => FALSE);
+   Used_Intervals : array (INTERVAL) of BOOLEAN := (others => FALSE);
+   -- Sets of notes and intervals in use
+
+   Vector: SOLUTION;
+   -- Vector with current (partial) solution
+
+   -- Try to add one step to the partial solution
+   procedure Extend(Current: LEVEL) is
       function  last_interval(c: CHOICE) return INTERVAL with Inline;
       function  reject(c: CHOICE) return BOOLEAN with Inline;
       procedure enter(c: CHOICE) with Inline;
@@ -46,7 +36,7 @@ procedure dodeca is
 
       -- compute choice/previous interval
       function last_interval(c: CHOICE) return INTERVAL is
-         previous: CHOICE renames serie(LEVEL'Pred(current));
+         previous: CHOICE renames Vector(LEVEL'Pred(Current));
       begin
          return INTERVAL(abs(c - previous));
       end;
@@ -54,56 +44,55 @@ procedure dodeca is
       -- reasons to prune
       function reject(c: CHOICE) return BOOLEAN is
       begin
-         return used_notes(c)
-            or else used_intervals(last_interval(c));
+         return Used_Notes(c)
+            or else Used_Intervals(last_interval(c));
       end;
 
       -- to wrap the recursive calls
       procedure enter(c: CHOICE) is
       begin
-         used_notes(c) := TRUE;
-         used_intervals(last_interval(c)) := TRUE;
+         Used_Notes(c) := TRUE;
+         Used_Intervals(last_interval(c)) := TRUE;
       end;
 
       procedure leave(c: CHOICE) is
       begin
-         used_intervals(last_interval(c)) := FALSE;
-         used_notes(c) := FALSE;
+         Used_Intervals(last_interval(c)) := FALSE;
+         Used_Notes(c) := FALSE;
       end;
 
    begin
-      -- try to extend solution with each choice
+      -- try to extend the solution with each choice
       for c in CHOICE loop
          if reject(c) then
             null; -- fail
          else
-            serie(current) := c;
+            Vector(Current) := c;
             -- at the bottom?
-            if current = LEVEL'Last then
-               output(serie);
+            if Current = LEVEL'Last then
+               Output(Vector);
             else
                -- descend one level
                enter(c);
-               extend(current => LEVEL'Succ(current));
+               Extend(Current => LEVEL'Succ(Current));
                leave(c);
             end if;
          end if;
       end loop;
-   end extend;
+   end Extend;
 
 begin
    declare
       first: constant := LEVEL'First;
    begin
       for c in CHOICE loop
-         serie(first) := c;
-         -- descend one level
-         used_notes(c) := TRUE;
-         extend(current => LEVEL'Succ(first));
-         used_notes(c) := FALSE;
+         Vector(first) := c;
+         Used_Notes(c) := TRUE;
+         Extend(Current => LEVEL'Succ(first));
+         Used_Notes(c) := FALSE;
       end loop;
    end;
-end dodeca;
+end Dodeca;
 
 -- ¡ISO-8859-1!
 -- vim:tabstop=3:shiftwidth=3:expandtab:autoindent
