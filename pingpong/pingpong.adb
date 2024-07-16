7faa81c8de56744d67e2d93e68a415be728b9e38
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2024-07-16 10:32:34 +0200

    Better procedural abstraction

--- pingpong/pingpong.adb
+++ pingpong/pingpong.adb
@@ -8,27 +8,27 @@ procedure pingpong is
    -- Private semaphore; works only for *two* tasks
    ---------------------------------------------------------------------
 
-   subtype Semaphore is Ada.Synchronous_Task_Control.Suspension_Object;
+   subtype SIGNAL is Ada.Synchronous_Task_Control.Suspension_Object;
 
-   -- Operations with `False` initialized semaphores (signals)
-   procedure Wait(S: in out Semaphore)
+   -- Operations with `False` initialized semaphores (SIGNALs)
+   procedure Wait(S: in out SIGNAL)
       renames Ada.Synchronous_Task_Control.Suspend_Until_True;
 
-   procedure Signal(S: in out Semaphore)
+   procedure Notify(S: in out SIGNAL)
       renames Ada.Synchronous_Task_Control.Set_True;
 
    -- Operations with `True` initialized semaphores (mutexes)
-   procedure Seize(S: in out Semaphore)
+   procedure Seize(S: in out SIGNAL)
       renames Ada.Synchronous_Task_Control.Suspend_Until_True;
 
-   procedure Release(S: in out Semaphore)
+   procedure Release(S: in out SIGNAL)
       renames Ada.Synchronous_Task_Control.Set_True;
 
    -- Make a new semaphore initizalized to `True`
-   function Make_Mutex return Semaphore is
+   function Make_Mutex return SIGNAL is
       use Ada.Synchronous_Task_Control;
    begin
-      return S:Semaphore do
+      return S:SIGNAL do
          -- by default suspension objects are set to `False`
          Set_True(S);
       end return;
@@ -38,51 +38,120 @@ procedure pingpong is
    --
    ---------------------------------------------------------------------
 
-   task type Ping_Routine(This, That: access Semaphore);
-   task type Pong_Routine(This, That: access Semaphore);
+   Conveyor_Error : exception;
 
-   task body Ping_Routine is
-      use Ada.Text_IO;
+   type CONVEYOR is
+      record
+         here: aliased SIGNAL; -- defaults to false
+         back: access  SIGNAL; -- defaults to null
+      end record;
+
+   procedure suspend(self: in out CONVEYOR) is
    begin
-      Wait (This.all);
+      Wait(self.here);
+   end suspend;
+
+   procedure call(self: in out CONVEYOR) is
+   begin
+      Notify(self.here);
+   end call;
+
+   procedure resume(self, other: in out CONVEYOR) is
+   begin
+      if self.back /= null then
+         other.back := self.back;
+      else
+         other.back := self.here'Unchecked_Access;
+      end if;
+      Notify(other.here);
+      Wait(self.here);
+   end resume;
+
+   procedure yield(self: in out CONVEYOR) is
+   begin
+      if self.back = null then
+         raise Conveyor_Error;
+      end if;
+      Notify(self.back.all);
+      Wait(self.here);
+   end yield;
+
+   ---------------------------------------------------------------------
+   --
+   ---------------------------------------------------------------------
+
+   task type Ping_Task(This, That: access SIGNAL);
+   task type Pong_Task(This, That: access SIGNAL);
+
+   ---------------------------------------------------------------------
+   --
+   ---------------------------------------------------------------------
+
+   task body Ping_Task is
+      use Ada.Text_IO;
+
+      procedure suspend with Inline is
+      begin
+         Wait(This.all);
+      end;
+
+      procedure resume(S: access SIGNAL; W: BOOLEAN := True) with Inline is
+      begin
+         Notify(S.all);
+         if W then
+            Wait(This.all);
+         end if;
+      end;
+
+   begin
+      suspend;
 
       for i in 1..10 loop
          Put("PING!  ");
-
-         Signal (That.all);
-         if i /= 10 then
-            Wait (This.all);
-         end if;
+         resume(That, i /= 10);
       end loop;
-   end Ping_Routine;
+   end Ping_Task;
 
-   task body Pong_Routine is
+   ---------------------------------------------------------------------
+   --
+   ---------------------------------------------------------------------
+
+   task body Pong_Task is
       use Ada.Text_IO;
+
+      procedure suspend with Inline is
+      begin
+         Wait(This.all);
+      end;
+
+      procedure resume(S: access SIGNAL) with Inline is
+      begin
+         Notify(S.all);
+         Wait(This.all);
+      end;
+
    begin
-      Wait (This.all);
+      suspend;
 
       for i in 1..10 loop
          Put_Line("PONG!");
-
          if i /= 10 then
-            Signal (That.all);
-            Wait   (This.all);
+            resume(That);
          end if;
       end loop;
-   end Pong_Routine;
+   end Pong_Task;
 
 begin
    ---------------------------------------------------------------------
    --
    ---------------------------------------------------------------------
    declare
-      ping_flag   : aliased Semaphore;
-      pong_flag   : aliased Semaphore;
-
-      ping_player : Ping_Routine (ping_flag'Access, pong_flag'Access);
-      pong_player : Pong_Routine (pong_flag'Access, ping_flag'Access);
+      ping : aliased SIGNAL;
+      pong : aliased SIGNAL;
+      ping_player : Ping_Task (ping'Access, pong'Access);
+      pong_player : Pong_Task (pong'Access, ping'Access);
    begin
-      Signal (ping_flag);
+      Notify(ping);
    end;
 end pingpong;
 
