8f12f51161a64b4182ff7b1855335bfca4433737
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2024-10-20 23:49:51 +0200

    Exploring Wrap

--- coroutines/control.ads
+++ coroutines/control.ads
@@ -71,10 +71,6 @@ package Control is
    -- Mandatory symmetric coroutines last call, except for the last to finish.
 
 private
-   ---------------------------------------------------------------------
-   -- Controller implementation
-   ---------------------------------------------------------------------
-
    type BASE_CONTROLLER is abstract tagged limited
       record
          id      : Ada.Task_Identification.TASK_ID;
@@ -85,11 +81,11 @@ private
       end record;
    -- := (Null_Task_Id, FALSE, NULL, NULL);
 
-   -- Stack like pass of control
    type ASYMMETRIC_CONTROLLER is new BASE_CONTROLLER with null record;
+   -- Stack like pass of control
 
-   -- Graph like pass of control
    type SYMMETRIC_CONTROLLER  is new BASE_CONTROLLER with null record;
+   -- Graph like pass of control
 
 end Control;
 
