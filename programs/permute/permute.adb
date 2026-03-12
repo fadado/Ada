7e3e97a128477556b86931fc3828a07235c6d9d8
Joan Josep Ordinas Rosa <jordinas@gmail.com>
2026-03-12 10:25:48 +0100

    Going generic...

--- programs/permute/permute.adb
+++ programs/permute/permute.adb
@@ -11,52 +11,75 @@ procedure Permute
 is
    use Control;
 
-   procedure Permute_Set   -- TODO add k- parameter ???
-     (Set : in STRING)
+   generic
+      Set : in STRING;
+      N   : in NATURAL := 0;
+   -- R   : BOOLEAN := FALSE; -- repeat?
+   package Sequences
+   is
+      Bits : constant := 8;
+
+      pragma Assert(N <= Set'Length);
+      pragma Assert(Set'First >= 0);
+      pragma Assert(Set'Last  <= 2**Bits-1);
+
+      ------------------------------------------------------------------
+      --
+      ------------------------------------------------------------------
+
+      type TINY is range 0..2**Bits-1;
+
+      First : constant TINY := TINY(Set'First);
+      Last  : constant TINY := TINY(Set'Last);
+
+      subtype VALUES is TINY range First..Last;
+
+      subtype INDICES is TINY
+         range First..(if N = 0 then Last else First+TINY(N)-1);
+
+      type SOLUTION is array (INDICES) of VALUES
+      with Pack,
+           Component_Size => Bits;
+
+      package DeclarePermutationsTypes is new Generators (
+         Element_Type => SOLUTION,
+         Context_Type => VOID
+      );
+      use DeclarePermutationsTypes;
+
+      procedure Generate
+        (generator : in out GENERATOR_INTERFACE'Class;
+         context   : access VOID);
+
+      subtype ITERABLE is GENERATOR_TYPE (Generate'Access, NULL);
+
+   end Sequences;
+
+   package body Sequences
    is
       ------------------------------------------------------------------
       --
       ------------------------------------------------------------------
 
-      Bits : constant := 8;
-
-      type TINY is range 0..2**Bits-1;
-
-      subtype INDICES is TINY range TINY(Set'First)..TINY(Set'Last);
-
-      type BIJECTION is array (INDICES) of INDICES
-      with Pack,
-           Component_Size => Bits;
-
-      package DeclarePermutationsTypes is new Generators (
-         Element_Type => BIJECTION,
-         Context_Type => VOID
-      );
-      use DeclarePermutationsTypes;
-
       type GENERATOR_PROXY is access all GENERATOR_INTERFACE'Class;
-
-      ------------------------------------------------------------------
-      --
-      ------------------------------------------------------------------
 
       Shared_Generator : GENERATOR_PROXY := NULL;
 
       procedure Goal
-        (solution : BIJECTION)
+        (vector : SOLUTION)
       with Inline,
            Pre => Shared_Generator /= NULL
       is
       begin
-         Shared_Generator.Yield(solution);
+         Shared_Generator.Yield(vector);
       end Goal;
 
-      Used_Items : array (INDICES) of BOOLEAN := (others => FALSE);
+      Used_Items : array (VALUES) of BOOLEAN := (others => FALSE);
 
       function Rejected
-        (candidate : BIJECTION;
+        (candidate : SOLUTION;
          index     : INDICES;
-         item      : INDICES) return BOOLEAN
+         item      : VALUES) return BOOLEAN
       with Inline,
            Pre => index > INDICES'First
       is
@@ -65,9 +88,9 @@ is
       end;
 
       procedure Enter
-        (candidate : BIJECTION;
+        (candidate : SOLUTION;
          index     : INDICES;
-         item      : INDICES)
+         item      : VALUES)
       with Inline,
            Pre  => not Used_Items(item),
            Post => Used_Items(item)
@@ -77,9 +100,9 @@ is
       end;
 
       procedure Leave
-        (candidate : BIJECTION;
+        (candidate : SOLUTION;
          index     : INDICES;
-         item      : INDICES)
+         item      : VALUES)
       with Inline,
            Pre  => Used_Items(item),
            Post => not Used_Items(item)
@@ -95,54 +118,54 @@ is
       procedure Generate
         (generator : in out GENERATOR_INTERFACE'Class;
          context   : access VOID)
-      with Pre  => Shared_Generator = NULL,
-           Post => Shared_Generator = NULL
       is
          package permuter is
             new Generics.Depth_First_Search (
                INDEX_TYPE   => INDICES,
-               ELEMENT_TYPE => INDICES,
-               ARRAY_TYPE   => BIJECTION
+               ELEMENT_TYPE => VALUES,
+               ARRAY_TYPE   => SOLUTION
             );
       begin
+         pragma Assert(Shared_Generator = NULL);
+
          Shared_Generator := generator'Unchecked_Access;
          permuter.Seek;
          Shared_Generator := NULL;
       end Generate;
 
-      subtype ITERABLE is GENERATOR_TYPE (Generate'Access, NULL);
-
-   begin
-      ------------------------------------------------------------------
-      --
-      ------------------------------------------------------------------
-
-      declare
-         use Ada.Text_IO;
-
-         permutations : ITERABLE;
-      begin
-
-      each_permutation:
-         for permutation of permutations loop
-
-         each_item:
-            for item of permutation loop
-               Put(' ');
-               Put(Set(POSITIVE(item)));
-            end loop each_item;
-
-            New_Line;
-         end loop each_permutation;
-
-      end;
-   end Permute_Set;
+   end Sequences;
 
 begin
-   -- main
-   Permute_Set("ABC");
+   ---------------------------------------------------------------------
+   --
+   ---------------------------------------------------------------------
 
-   -- Output for RGB:
+main:
+   declare
+      use Ada.Text_IO;
+
+      str : constant STRING := "ABC";
+
+      package P is new Sequences(str, 3);
+
+      permutations : P.ITERABLE;
+   begin
+
+   each_permutation:
+      for permutation of permutations loop
+
+      each_index:
+         for index of permutation loop
+            Put(' ');
+            Put(str(POSITIVE(index)));
+         end loop each_index;
+
+         New_Line;
+      end loop each_permutation;
+
+   end main;
+
+   -- Output for "RGB":
    --    R G B
    --    R B G
    --    G R B
@@ -152,6 +175,5 @@ begin
 
 end Permute;
 
--- ¡ISO-8859-1!
 -- vim:tabstop=3:shiftwidth=3:expandtab:autoindent
--- vim:fileformat=dos:fileencoding=latin1:syntax=ada
+-- vim:fileformat=dos:fileencoding=UTF8:syntax=ada
--- programs/series/series.adb
+++ programs/series/series.adb
@@ -110,6 +110,5 @@ begin
    end;
 end Series;
 
--- ¡ISO-8859-1!
 -- vim:tabstop=3:shiftwidth=3:expandtab:autoindent
--- vim:fileformat=dos:fileencoding=latin1:syntax=ada
+-- vim:fileformat=dos:fileencoding=UTF8:syntax=ada
